6eb6950b9f2378e14c725343fb7327c29cece39b
deanwithadream <allyssa@deanwithadream.com>
2026-03-14 12:45:25 -0600

    Update videos.js

--- netlify/functions/videos.js
+++ netlify/functions/videos.js
@@ -15,18 +15,25 @@ exports.handler = async function(event) {
   }
 
   try {
-    // Search by name instead of handle
     const channelRes = await fetchURL(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=Jude+and+Missy+Pie+God+First&type=channel&maxResults=5&key=${API_KEY}`
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&forHandle=god1stJudeandMissyPie&key=${API_KEY}`
     );
 
-    // Return raw results so we can see what YouTube finds
+    if (!channelRes.items || channelRes.items.length === 0) {
+      throw new Error('Channel not found');
+    }
+
+    const channelId = channelRes.items[0].id;
+
+    const videos = await fetchURL(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=8&order=date&type=video&key=${API_KEY}`
+    );
+
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify(channelRes)
+      body: JSON.stringify(videos)
     };
-
   } catch (e) {
     return {
       statusCode: 500,
